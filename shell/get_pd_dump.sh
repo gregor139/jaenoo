7e76c920455855493ca8e357296f28443b4ecb19
gregor139 <gregor@duerrs.ch>
2016-10-08 22:48:43 +0200

    Update get_pd_dump.sh

--- shell/get_pd_dump.sh
+++ shell/get_pd_dump.sh
@@ -3,5 +3,9 @@
 # Author: Gregor Duerr, Switzerland
 
 cd /Users/gregor/jaenoo/data_Files/Personen/
-# wget -m https://tools.wmflabs.org/persondata/data/pd_dump.txt > pd_dump.txt
+
+# For MacOS
 curl -# https://tools.wmflabs.org/persondata/data/pd_dump.txt > pd_dump.txt
+
+# For Linux
+# wget -m https://tools.wmflabs.org/persondata/data/pd_dump.txt > pd_dump.txt
