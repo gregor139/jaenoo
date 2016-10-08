294250e08ac5f95d2dc315e376de6a5c71a6745a
Gregor Duerr <gregor@duerrs.ch>
2016-10-08 22:46:55 +0200

    Initial version

---
+++ shell/get_pd_dump.sh
@@ -0,0 +1,7 @@
+# jaenoo
+# Time Machine
+# Author: Gregor Duerr, Switzerland
+
+cd /Users/gregor/jaenoo/data_Files/Personen/
+# wget -m https://tools.wmflabs.org/persondata/data/pd_dump.txt > pd_dump.txt
+curl -# https://tools.wmflabs.org/persondata/data/pd_dump.txt > pd_dump.txt
